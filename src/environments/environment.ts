1945d10bf22e936f6e5a334ccf882324d1a7b94c
opo⅃ <lokmasazali@gmail.com>
2025-08-28 11:00:49 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: false,
     API_URL: 'http://127.0.0.1:5000',
     API_KEY: '123456789',
-    CSV_BASE: '/csv'
-  };
+    CSV_BASE: '/csv',
+    NOTION_CLIENT_ID: ""
+};
   
