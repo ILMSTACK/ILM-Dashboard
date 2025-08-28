44305c12dcdddbc9ffc69a1a142ab8beee27df02
opo⅃ <lokmasazali@gmail.com>
2025-08-28 11:07:40 +0800

    Update environment.production.ts

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     API_URL: 'http://localhost:8081',
-    API_KEY: '123456789'
-    NOTION_CLIENT_ID: ""
+    API_KEY: '123456789',
+    NOTION_CLIENT_ID: ''
   };
   
